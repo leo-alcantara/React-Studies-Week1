4d485bcf4ceef0e38bc407e1d06438692778020b
leoodindealcantara <leoalcantara@icloud.com>
2021-11-08 15:15:31 +0100

    first commit

---
+++ src/ArrowFunctionApp/ArrowFunctionApp.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+
+
+const Header = () => {
+    return (<div>Header</div>);
+};
+
+const Content = () => {
+    return(<div>Content</div>)
+
+};
+
+const Footer = () => {
+    return (<div>Footer</div>)
+};
+
+
+const ArrowFunctionApp = () => {
+    return (
+        <div>
+            <Header/>
+            <Content/>
+            <Footer/>
+        </div>
+    );
+};
+
+export default ArrowFunctionApp;
